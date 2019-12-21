5a1b36965d4a0e4605f6c67f43181d9dbbb00695
sam <yumedream12@gmail.com>
2019-12-21 23:16:22 +0700

    Add divide command for calculator
    Divide feature to be used on the application.

---
+++ src/Commands/DivideCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Jakmall\Recruitment\Calculator\Commands;
+
+use Illuminate\Console\Command;
+
+class DivideCommand extends CalculatorCommand
+{
+    
+    public function __construct()
+    {
+        $signature = $this->getSignature();
+        $description = $this->getDescription();
+        $operator = $this->getOperator();
+
+        parent::__construct($this->getCommandVerb(), $signature, $description, $operator);
+    }
+
+    protected function getCommandVerb(): string
+    {
+        return 'divide';
+    }
+
+    protected function getCommandPassiveVerb(): string
+    {
+        return 'divided';
+    }
+
+    protected function getOperator(): string
+    {
+        return '/';
+    }
+    
+    protected function getSignature(): string
+    {
+        $string = sprintf(
+            '%s {numbers* : The numbers to be %s}',
+            $this->getCommandVerb(),
+            $this->getCommandPassiveVerb()
+        );
+
+        return $string;
+    }
+
+    public function getDescription(): string 
+    {
+        $string = sprintf('%s all given Numbers', ucfirst($this->getCommandVerb()));
+
+        return $string;
+    }
+    
+}
